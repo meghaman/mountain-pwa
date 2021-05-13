035508aa15f383c8e675133ce55d1b76345ead69
meghaman <mkulacha@gmail.com>
2021-05-13 14:04:39 -0400

    Added react-geolocate and integrated with Map component

--- src/Components/Pages/MapPage.tsx
+++ src/Components/Pages/MapPage.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { geolocated, GeolocatedProps } from "react-geolocated";
 
-const mapPage = () => {
-  return <div>Map Page</div>;
+const MapPage: React.FC<GeolocatedProps> = (props) => {
+  let longitude = 0;
+  let latitude = 0;
+
+  if (props && props.coords) {
+    longitude = props.coords.longitude;
+    latitude = props.coords.latitude;
+  }
+
+  return (
+    <div>
+      Longitude: {longitude}, Latitude: {latitude}
+    </div>
+  );
 };
 
-export default mapPage;
+export default geolocated({
+  watchPosition: true,
+})(MapPage);
